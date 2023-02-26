a71014237c592676c504bc66a51c5a473244990d
tomegavazovv <tomegavazov@gmail.com>
2023-02-26 13:34:57 +0100

    added location service

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:path/path.dart' as path;
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
-import 'package:stumble/hooks/blockUser.dart';
-import 'package:stumble/hooks/fetchNearbyUsers.dart';
-import 'package:stumble/hooks/login.dart';
-import 'package:stumble/hooks/register.dart';
-import 'package:stumble/oauth/GoogleAuth.dart';
 import 'package:stumble/screens/chat_screen.dart';
 
+import 'services/locationService.dart';
 import 'hooks/fetchUserDetails.dart';
+import 'hooks/locate.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,33 +20,53 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Stumble',
+    return MaterialApp(
+      title: 'Stumble',
       theme: ThemeData(
-        primarySwatch: Colors.blue, primaryColor: Colors.lightBlueAccent[200],),
-      home: const MyHomePage(),);
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.lightBlueAccent[200],
+      ),
+      home: const MyHomePage(),
+    );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final ButtonStyle style = TextButton.styleFrom(foregroundColor: Theme
-        .of(context)
-        .colorScheme
-        .onPrimary,);
-    return Scaffold(appBar: AppBar(title: const Text('Stumble Home Page'),
-        actions: <Widget>[TextButton(style: style, onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => ChatScreen()));
-        }, child: const Text('Chat'),)
-        ]), body: FloatingActionButton.small(onPressed: () {
-      blockUser('tomegavazov2@gmail.com',
-          'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRvbWVnY'
-              'XZhem92QGdtYWlsLmNvbSJ9.ApB8kTzXfHQTq-Xhcmc'
-              'XfudIeiwL032EIwevthYHnDo');
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-    }));
+class _MyHomePageState extends State<MyHomePage> {
+  Position? _currentPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = TextButton.styleFrom(
+      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+    );
+    return Scaffold(
+        appBar:
+            AppBar(title: const Text('Stumble Home Page'), actions: <Widget>[
+          TextButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ChatScreen()));
+            },
+            child: const Text('Chat'),
+          )
+        ]),
+        body: Container(
+            child: ElevatedButton(
+          onPressed: () {
+            getCurrentPosition(context).then((position) => setState(() {
+                  _currentPosition = position;
+                }));
+          },
+          child: Text("Lat: ${_currentPosition?.latitude.toString() ?? ''} "
+              "Lon: ${_currentPosition?.longitude.toString() ?? ''}"),
+        )));
   }
 }
