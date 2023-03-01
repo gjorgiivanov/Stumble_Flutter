3439d5affb075c1e738bc00e4c0f2567fefe480e
tomegavazovv <tomegavazov@gmail.com>
2023-03-01 10:17:09 +0100

    chat_list

--- lib/hooks/blockUser.dart
+++ lib/hooks/blockUser.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:stumble/models/UserDetails.dart';
 
 void blockUser(int userId, blockUserId) async {
   await http.post(
