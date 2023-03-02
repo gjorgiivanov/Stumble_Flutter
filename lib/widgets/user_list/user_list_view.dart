8bb6c4faa6cf394aba7525c21d648f5acca7a5a6
gorgiivanov <gorgi.ivanov01@gmail.com>
2023-03-02 17:26:04 +0100

    STUM-6 <image display fix>

--- lib/widgets/user_list/user_list_view.dart
+++ lib/widgets/user_list/user_list_view.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 import '../../providers/users.dart';
@@ -13,18 +11,6 @@ class UserList extends StatefulWidget {
 }
 
 class _UserListState extends State<UserList> {
-  final ref = FirebaseStorage.instance.ref().child("user_images");
-
-  Future<ImageProvider?> _getImageUrl(String email) async {
-    var img = ref.child(email + '.jpg');
-
-    if (img.isBlank != null && !img.isBlank!) {
-      var downloadUrl = await img.getDownloadURL();
-      return NetworkImage(downloadUrl);
-    }
-    return null;
-  }
-
   @override
   Widget build(BuildContext context) {
     final usersData = Provider.of<Users>(context).items;
@@ -34,17 +20,12 @@ class _UserListState extends State<UserList> {
       itemBuilder: (BuildContext context, int index) {
         final user = usersData[index];
         return ListTile(
-          leading: FutureBuilder(
-            future: _getImageUrl(user.email),
-            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-              return CircleAvatar(
-                radius: 30,
-                backgroundImage: snapshot.data,
-                child: Text(
-                  user.firstName,
-                ),
-              );
-            },
+          leading: CircleAvatar(
+            radius: 30,
+            backgroundImage: NetworkImage(user.image),
+            child: Text(
+              user.firstName,
+            ),
           ),
           title: Text('${user.firstName} ${user.lastName}'),
           subtitle: Row(
